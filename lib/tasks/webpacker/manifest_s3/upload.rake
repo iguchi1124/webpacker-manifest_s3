065e3ba8158b35dd1365ab842a5bd777a391cbdd
Shota Iguchi <shota-iguchi@cookpad.com>
2017-06-26 18:35:37 +0900

    Implement upload task

--- lib/tasks/webpacker/manifest_s3/upload.rake
+++ lib/tasks/webpacker/manifest_s3/upload.rake
@@ -1,5 +1,8 @@
 desc 'upload manifest file to AWS S3'
 namespace 'webpacker:manifest_s3' do
-  task upload: :environment do
+  task :upload do
+    Webpacker::Env.load
+    Webpacker::Configuration.load
+    Webpacker::ManifestS3::S3Client.put_manifest
   end
 end
--- lib/webpacker/manifest_s3/manifest_extention.rb
+++ lib/webpacker/manifest_s3/manifest_extention.rb
@@ -1,5 +1,3 @@
-require 'aws-sdk'
-
 module Webpacker
   module ManifestS3
     module ManifestExtention
@@ -7,43 +5,12 @@ module Webpacker
 
       def load
         if Webpacker::Configuration.manifest_s3_bucket && Webpacker::Configuration.manifest_s3_key
-          resp = s3_client.get_object(bucket: Webpacker::Configuration.manifest_s3_bucket, key: Webpacker::Configuration.manifest_s3_key)
+          resp = Webpacker::ManifestS3::S3Client.get_manifest
           JSON.parse(resp.body.read)
         else
           return super unless File.exist?(@path)
           JSON.parse(File.read(@path))
         end
-      end
-
-      def s3_client
-        Aws::S3::Client.new(client_options)
-      end
-
-      def client_options
-        hash = {}
-        hash[:credentials] = credentials
-        hash[:region] = region if region
-        hash
-      end
-
-      def credentials
-        if access_key_id && secret_access_key
-          Aws::Credentials.new(access_key_id, secret_access_key)
-        else
-          Aws::InstanceProfileCredentials.new
-        end
-      end
-
-      def access_key_id
-        Webpacker::Configuration.aws_access_key_id || ENV['AWS_ACCESS_KEY_ID']
-      end
-
-      def secret_access_key
-        Webpacker::Configuration.aws_secret_access_key || ENV['AWS_SECRET_ACCESS_KEY']
-      end
-
-      def region
-        Webpacker::Configuration.aws_region || ENV['AWS_REGION']
       end
     end
   end
